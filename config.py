98628304cfcf863246f5eb15fe316ab859195308
Greemv <hvitl@mail.ru>
2025-09-29 15:17:09 +0500

    fx

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ class Config:
     # MySQL через Docker на порту 3307
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "mysql+pymysql://user@127.0.0.1:3306/db"
+        "mysql+pymysql://user:userpass@127.0.0.1:3307/newsdb"
     )
 
     # Postgres + pgvector через Docker на порту 5433
     POSTGRES_URI = os.getenv(
         "POSTGRES_URI",
-        "postgresql+psycopg2://postgres@127.0.0.1:5432/db"
+        "postgresql+psycopg2://postgres:123@127.0.0.1:5433/newsdb"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
